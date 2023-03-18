2f1935c420839de4fde7a48bbe0bcc86f9e38598
ana <andelamo@myuax.com>
2023-03-18 15:16:30 +0100

    Comiezo de main

--- src/main/java/Catalogo.java
+++ src/main/java/Catalogo.java
@@ -1,5 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
 public class Catalogo {
     public static void main(String[] args) {
+        ArrayList<Ciclos> catalogo = new ArrayList<Ciclos>();
+
+        catalogo.add(new Bicicleta("Lapierre", "400",27, LocalDate.of(2022,3,1)));
+        catalogo.add(new Bicicleta("Btwin", "900", 10, LocalDate.of(2013,1,1)));
+        catalogo.add(new Giroscopio("Segway", "Nine", 40,  LocalDate.of(2023,2,1)));
+        catalogo.add(new Giroscopio("Weebot", "Echo", 35, LocalDate.of(2022, 12,1)));
+        catalogo.add(new Segway("Immotion", "v8", 40, LocalDate.of(2023, 3,1)));
+        catalogo.add(new Segway("Segway", "Ninebot One E+", 30, LocalDate.of(2023, 1,1)));
+
+        System.out.println("Est es el catálogo de ciclos: ");
+        System.out.printf("%-15s%-25s%-30s%s\n", "Tipo", "Marca", "Autonomia", "Tarifa");
+        for (Ciclos ciclo : catalogo){
+            System.out.println(ciclo);
+        }
+
 
     }
 }
--- src/main/java/Segway.java
+++ src/main/java/Segway.java
@@ -4,7 +4,7 @@ public class Segway extends Ciclos {
     private double autonomia;
     private double alturaMinima;
 
-    public Segway(String marca, String modelo, double autonomia, double alturaMinima, LocalDate fechaCompra) {
+    public Segway(String marca, String modelo, double autonomia, LocalDate fechaCompra) {
         super(marca, modelo, fechaCompra);
         this.autonomia = autonomia;
         this.alturaMinima = alturaMinima;
