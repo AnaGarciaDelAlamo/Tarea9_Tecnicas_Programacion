079c8475e8a1689edb0c1b33b8027df4c53d9180
ana <andelamo@myuax.com>
2023-03-18 20:34:16 +0100

    Ejercicio2 con main

--- src/main/java/Ejercicio2/Main.java
+++ src/main/java/Ejercicio2/Main.java
@@ -4,9 +4,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        String idioma = JOptionPane.showInputDialog("Seleccione un idioma: ");
-        System.out.println("1.-Español");
-        System.out.println("2.- Inglés");
+        String idioma = JOptionPane.showInputDialog("Seleccione un idioma 1-Español, 2-Inglés: ");
 
         Traduccion traduccion;
         if (idioma.equals("1")) {
@@ -19,7 +17,7 @@ public class Main {
         double distancia = Double.parseDouble(JOptionPane.showInputDialog("Introduzca la distancia recorrida en km: "));
         traduccion.introducirTiempo();
         double tiempo = Double.parseDouble(JOptionPane.showInputDialog("Introduzca el tiempo de viaje en h: "));
-        double velocidad = (distancia / tiempo) * 60;
+        double velocidad = (distancia / tiempo);
 
         traduccion.inicioRespuesta();
         traduccion.finalRespuesta(velocidad);
